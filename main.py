5e5f05b8f3a1137714919683d83440b74e421f12
Gaurav Arora <gauravarora011@gmail.com>
2017-06-21 03:11:35 +0530

    fucntional api

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,render_template
 from data import prices,fuel_type
 from Scrapper import scrapper
 
 app = Flask(__name__)
 
-@app.route('/<city>')
+@app.route('/v1/cities')
+def cities():
+    return jsonify({'cities': prices.keys()})
+
+@app.route('/v1/fuel-types')
+def fuel_list():
+    return jsonify({'fuel-types': fuel_type})
+
+@app.route('/v1/price/<city>')
 def city_price(city):
     city = city.lower()
     if city in prices.keys():
@@ -12,9 +20,20 @@ def city_price(city):
     else:
         return "city not found"
 
+@app.route('/v1/price/<city>/<fuel>')
+def city_fuel_price(city,fuel):
+    city = city.lower()
+    if fuel not in fuel_type:
+        return "Fuel Type Invalid"
+    if city in prices.keys():
+        return jsonify({'price': prices[city.lower()][fuel]})
+    else:
+        return "city not found"
+
 @app.route('/')
 def hello_world():
-    return "Welcome"
+    return render_template('index.html',city_list=prices.keys(),fuel_type=fuel_type)
+    #return "Welcome"
 
 if __name__ == "__main__":
     scrapper()
